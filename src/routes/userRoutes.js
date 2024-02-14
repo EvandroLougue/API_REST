53aeacda606d2b7c2c4117a088e02d7cd7488cd0
Evandro Lougue <evandrolougue@gmail.com>
2024-02-14 18:14:27 -0300

    update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,11 +3,14 @@ import userController from '../controllers/UserController';
 
 const router = new Router();
 
-router.get('/', userController.index);
+router.get('/', userController.store);
 
 export default router;
 
 /*
-.index => listar
-.store/.create => criar
+.index => listar todos => GET
+.store/.create => criar novo => POST
+.delete => apaga um => DELETE
+.show => mostra um
+.update => atualiza um
 */
